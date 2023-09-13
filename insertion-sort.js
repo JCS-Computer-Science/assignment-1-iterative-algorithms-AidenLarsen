3904f35bcfb3ded799e7ef7636215b2a9d1d13dc
Aiden Larsen <16118004@edu.cbe.ab.ca>
2023-09-13 09:49:45 -0600

    insertion sort complete

--- insertion-sort.js
+++ insertion-sort.js
@@ -5,6 +5,23 @@ function insertionSort(array) {
 	 ** is bigger
 	 */
 
+	 let x;
+	 let c;
+
+	 while(c !== 0) {
+		 c = 0;
+		for(let i = 1; i < array.length; i++) {
+			if(array[i] < array[i-1]){
+				 x = array[i];
+				 array[i] = array[i-1];
+				 array[i-1] = x;
+				 c++;
+			 }
+		 }
+	 }
+	
+	 
+
 	return array;
 }
 
